04eba5ed79fccc8a5a6f96060735cb56129cad5c
bhi049 <bhi049@myy.haaga-helia.fi>
2025-05-30 16:15:05 +0300

    Basic component setup and router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Favourites from './pages/Favourites'
 
 function App() {
 
   return (
-    <div></div>
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/favourites" element={<Favourites />}/>
+      </Routes>
+    </main>
   )
 }
 
---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,10 @@
+function Favourites() {
+  return (
+    <div className="favourites-page">
+      <h1>Favourites</h1>
+      <p>This is the Favourites page.</p>
+    </div>
+  );
+}
+
+export default Favourites;
